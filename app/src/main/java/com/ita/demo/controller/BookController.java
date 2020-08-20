9c8ba8c8aec7f128180f571468b5df8fc0e26ecc
alex <alex.h.z.xu@oocl.com>
2020-08-20 14:58:58 +0800

    [alex] feature: add get

---
+++ app/src/main/java/com/ita/demo/controller/BookController.java
@@ -0,0 +1,45 @@
+package com.ita.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/orders")
+@Slf4j
+public class BookController {
+    protected static final String X_GSBN_APPLICATION = "x-gsbn-application";
+    protected static final String X_GSBN_ORG = "x-gsbn-org";
+    protected static final String X_GSBN_ORG_ROLE = "x-gsbn-org-role";
+
+    @Value("${gsbn.host}")
+    protected String apiHost;
+    @Value("${gsbn.application-id}")
+    protected String gsbnApplicationId;
+    @Value("${gsbn.org-id}")
+    protected String gsbnOrgId;
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("{assetId}")
+    public ResponseEntity<String> getOrder(@PathVariable Integer assertId) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(X_GSBN_ORG_ROLE, "Shipper");
+        httpHeaders.set(X_GSBN_ORG, gsbnOrgId);
+        httpHeaders.set(X_GSBN_APPLICATION, gsbnApplicationId);
+        HttpEntity<Object> stringHttpEntity = new HttpEntity<>(httpHeaders);
+        return restTemplate.exchange(apiHost + "/documents/bookingRequest", HttpMethod.POST, stringHttpEntity, String.class);
+    }
+
+}
